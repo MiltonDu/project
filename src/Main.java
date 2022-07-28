e1077a2496a14c5ed65abd649cb3e0804b7b4ca7
MiltonDu <n00263188@upn.pe>
2022-07-28 10:59:18 -0500

    Ejercicio completado

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        coche miCoche = new coche();
+        miCoche.aumentarPuertas();
+        System.out.println("Numero de puertas: "+ miCoche.puerta);
+
+        int param_a = 5;
+        int param_b = 6;
+        int param_c = 7;
+        var resultado = suma(param_a, param_b, param_c);
+        System.out.println("Suma = "+ resultado);
+    }
+
+    public static int suma (int a, int b, int c){
+        return a + b + c;
+    }
+}
+
+class coche {
+    public int puerta = 0;
+
+    public void aumentarPuertas(){
+        this.puerta++;
     }
 }
